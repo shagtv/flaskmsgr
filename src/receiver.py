e631df954f57fc0a709bc6fe8927c7c59deb1a8d
Timur Shagiakhmetov <timur.shagiakhmetov@corp.badoo.com>
2019-11-20 01:11:30 +0000

    fixed code style

--- src/receiver.py
+++ src/receiver.py
@@ -5,8 +5,7 @@ from datetime import datetime
 messages_after = 0
 
 while True:
-    response = requests.get('http://127.0.0.1:5000/messages',
-                             params={'after': messages_after})
+    response = requests.get('http://127.0.0.1:5000/messages', params={'after': messages_after})
     if response.status_code == 200:
         messages = response.json().get('messages')
         for message in messages:
