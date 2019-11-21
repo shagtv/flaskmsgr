ef832902657be0079228fa2d0a81a962930366ec
Timur Shagiakhmetov <timur.shagiakhmetov@corp.badoo.com>
2019-11-21 00:51:21 +0000

    added client

---
+++ design.py
@@ -0,0 +1,91 @@
+# -*- coding: utf-8 -*-
+
+# Form implementation generated from reading ui file 'design.ui'
+#
+# Created by: PyQt5 UI code generator 5.13.2
+#
+# WARNING! All changes made in this file will be lost!
+
+
+from PyQt5 import QtCore, QtGui, QtWidgets
+
+
+class Ui_MainWindow(object):
+    def setupUi(self, MainWindow):
+        MainWindow.setObjectName("MainWindow")
+        MainWindow.resize(800, 750)
+        self.centralwidget = QtWidgets.QWidget(MainWindow)
+        self.centralwidget.setObjectName("centralwidget")
+        self.label = QtWidgets.QLabel(self.centralwidget)
+        self.label.setGeometry(QtCore.QRect(230, 40, 261, 91))
+        font = QtGui.QFont()
+        font.setPointSize(36)
+        self.label.setFont(font)
+        self.label.setObjectName("label")
+        self.lineEdit = QtWidgets.QLineEdit(self.centralwidget)
+        self.lineEdit.setGeometry(QtCore.QRect(120, 148, 131, 28))
+        font = QtGui.QFont()
+        font.setPointSize(16)
+        font.setBold(False)
+        font.setWeight(50)
+        self.lineEdit.setFont(font)
+        self.lineEdit.setText("")
+        self.lineEdit.setObjectName("lineEdit")
+        self.lineEdit_2 = QtWidgets.QLineEdit(self.centralwidget)
+        self.lineEdit_2.setGeometry(QtCore.QRect(380, 150, 171, 28))
+        font = QtGui.QFont()
+        font.setPointSize(16)
+        self.lineEdit_2.setFont(font)
+        self.lineEdit_2.setObjectName("lineEdit_2")
+        self.label_2 = QtWidgets.QLabel(self.centralwidget)
+        self.label_2.setGeometry(QtCore.QRect(60, 150, 61, 30))
+        font = QtGui.QFont()
+        font.setPointSize(16)
+        self.label_2.setFont(font)
+        self.label_2.setObjectName("label_2")
+        self.label_3 = QtWidgets.QLabel(self.centralwidget)
+        self.label_3.setGeometry(QtCore.QRect(280, 150, 111, 30))
+        font = QtGui.QFont()
+        font.setPointSize(16)
+        self.label_3.setFont(font)
+        self.label_3.setObjectName("label_3")
+        self.textBrowser = QtWidgets.QTextBrowser(self.centralwidget)
+        self.textBrowser.setGeometry(QtCore.QRect(60, 190, 691, 411))
+        self.textBrowser.setObjectName("textBrowser")
+        self.plainTextEdit = QtWidgets.QPlainTextEdit(self.centralwidget)
+        self.plainTextEdit.setEnabled(False)
+        self.plainTextEdit.setGeometry(QtCore.QRect(60, 620, 431, 75))
+        font = QtGui.QFont()
+        font.setPointSize(16)
+        self.plainTextEdit.setFont(font)
+        self.plainTextEdit.setObjectName("plainTextEdit")
+        self.pushButton = QtWidgets.QPushButton(self.centralwidget)
+        self.pushButton.setEnabled(False)
+        self.pushButton.setGeometry(QtCore.QRect(530, 621, 221, 75))
+        font = QtGui.QFont()
+        font.setPointSize(21)
+        self.pushButton.setFont(font)
+        self.pushButton.setObjectName("pushButton")
+        self.pushButton_2 = QtWidgets.QPushButton(self.centralwidget)
+        self.pushButton_2.setEnabled(True)
+        self.pushButton_2.setGeometry(QtCore.QRect(580, 150, 111, 28))
+        font = QtGui.QFont()
+        font.setPointSize(16)
+        self.pushButton_2.setFont(font)
+        self.pushButton_2.setObjectName("pushButton_2")
+        MainWindow.setCentralWidget(self.centralwidget)
+        self.statusbar = QtWidgets.QStatusBar(MainWindow)
+        self.statusbar.setObjectName("statusbar")
+        MainWindow.setStatusBar(self.statusbar)
+
+        self.retranslateUi(MainWindow)
+        QtCore.QMetaObject.connectSlotsByName(MainWindow)
+
+    def retranslateUi(self, MainWindow):
+        _translate = QtCore.QCoreApplication.translate
+        MainWindow.setWindowTitle(_translate("MainWindow", "MainWindow"))
+        self.label.setText(_translate("MainWindow", "Messenger"))
+        self.label_2.setText(_translate("MainWindow", "Login"))
+        self.label_3.setText(_translate("MainWindow", "Password"))
+        self.pushButton.setText(_translate("MainWindow", "Send"))
+        self.pushButton_2.setText(_translate("MainWindow", "login"))
---
+++ messenger.py
@@ -0,0 +1,80 @@
+import sys
+from PyQt5 import QtWidgets
+from design import Ui_MainWindow
+import requests
+from datetime import datetime
+import time
+import threading
+
+
+url = sys.argv[1] if len(sys.argv) > 1 else 'http://127.0.0.1:5000/'
+
+
+class ExampleApp(QtWidgets.QMainWindow, Ui_MainWindow):
+    def __init__(self):
+        super().__init__()
+        self.setupUi(self)
+        self.pushButton_2.clicked.connect(self.login)
+        self.pushButton.clicked.connect(self.send)
+
+        quit = QtWidgets.QAction("Quit", self)
+        quit.triggered.connect(self.closeEvent)
+
+        threading.Thread(target=self.receive).start()
+
+    def closeEvent(self, event):
+        username = self.lineEdit.text()
+        password = self.lineEdit_2.text()
+        requests.post(url + 'logout',
+                      json={'username': username, 'password': password})
+        event.accept()
+
+    def login(self):
+        username = self.lineEdit.text()
+        password = self.lineEdit_2.text()
+
+        if len(username) == 0 or len(password) == 0:
+            return
+
+        response = requests.post(url + 'login',
+                                 json={'username': username, 'password': password})
+        if response.status_code == 200:
+            self.lineEdit.setEnabled(False)
+            self.lineEdit_2.setEnabled(False)
+            self.pushButton_2.setEnabled(False)
+            self.plainTextEdit.setEnabled(True)
+            self.pushButton.setEnabled(True)
+
+    def send(self):
+        username = self.lineEdit.text()
+        password = self.lineEdit_2.text()
+        text = self.plainTextEdit.toPlainText()
+
+        if len(username) == 0 or len(password) == 0 or len(text) == 0:
+            return
+
+        response = requests.post(url + 'send',
+                                 json={'username': username, 'password': password, 'text': text})
+        if response.status_code == 200:
+            self.plainTextEdit.setPlainText('')
+
+    def receive(self):
+        messages_after = 0
+        while True:
+            response = requests.get(url + 'messages', params={'after': messages_after})
+            if response.status_code == 200:
+                messages = response.json().get('messages')
+                for message in messages:
+                    time_str = datetime.fromtimestamp(message['time']).strftime('%Y-%m-%d %H:%M:%S')
+                    self.textBrowser.append(message['username'] + ': ' + time_str)
+                    self.textBrowser.append(str(message['text']))
+                    self.textBrowser.append('')
+                    messages_after = message['time']
+            time.sleep(1)
+
+
+if __name__ == '__main__':
+    app = QtWidgets.QApplication([])
+    window = ExampleApp()
+    window.show()
+    app.exec_()
